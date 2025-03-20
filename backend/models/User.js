c45e59eb882455ff190491209de61c05d134bf54
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-03-20 21:56:23 +0530

    created schema for the user

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const UserSchema=new mongoose.Schema(
+    {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: "" },
+    location: { type: String, required: true }, 
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Users this user follows
+  }, { timestamps: true }
+)
+
+module.exports = mongoose.model("User", UserSchema);
