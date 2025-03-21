6744273c3f3b57a8cdf5ca1181363b9aec5855e0
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-03-21 22:46:55 +0530

    connected to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./config/db");
-
-dotenv.config();
-connectDB();
+const postRoutes = require("./routes/postRoutes"); // Import post routes
 
 const app = express();
+dotenv.config();
+
 app.use(express.json());
 app.use(cors());
+app.use("/api/posts", postRoutes); // Register routes
 
-// // Routes
-// app.use("/api/posts", require("./routes/postRoutes"));
+mongoose.connect(process.env.MONGO_URI, { dbName: 'communityDB' })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+  mongoose.connection.on('connected', () => {
+    console.log("Connected to MongoDB database:", mongoose.connection.name);
+  });
+  
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
