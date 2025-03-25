100bfc8ca8e4e9360f590cd57c01d7d6cab500cb
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-03-25 22:27:20 +0530

    created explore page

---
+++ frontend/src/pages/Explore.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+
+
+// const Explore = () => {
+
+//     const [posts, setPosts] = useState([]);
+//     useEffect(()=>{
+//         axios.get("http://localhost:5000/api/posts")
+//         .then((res)=>{setPosts(res)})
+//         .catch((err)=>{console.log(err)})
+//     },[])
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default Explore
