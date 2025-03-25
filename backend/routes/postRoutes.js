0f625eee55d43f9b73f73d9dd7fc419814c9f81e
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-03-25 22:27:40 +0530

    deleted

--- backend/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-
-const router = express.Router();
-const { getFollowingPosts } = require('../controllers/postController');
-router.get('/following/:userId', getFollowingPosts);
-
-module.exports = router;
