b6d200f7bca3e9fc91cb44ae062d914b76f65e3d
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-03-30 11:22:45 +0530

    modified to get both reels and posts

--- backend/controllers/getpostsController.js
+++ backend/controllers/getpostsController.js
@@ -1,16 +1,30 @@
 const Post = require("../models/Posts");
+const Reels = require("../models/Reels");
 
-
-const getpostsController = async (req, res) => {
+const getPostsAndReelsController = async (req, res) => {
   try {
+    
     const posts = await Post.find()
-      .populate("userId", "username profilePic location")
-      .sort({ createdAt: -1 });
-    res.status(200).json(posts);
+    .populate("userId", "username profilePic location") // Fetch user data for each post
+    .sort({ createdAt: -1 });
+  
+  const reels = await Reels.find()
+    .populate("userId", "username profilePic location") // Fetch user data for each reel
+    .sort({ createdAt: -1 });
+  
+
+    // Combine the posts and reels into one array
+    const combinedContent = [...posts, ...reels];
+
+    // Sort combined content by creation date in descending order
+    combinedContent.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+    // Send the combined array of posts and reels
+    res.status(200).json(combinedContent);
   } catch (err) {
     console.error("Server Error:", err);
     res.status(500).json({ message: "Server Error" });
   }
 };
 
-module.exports = { getpostsController };  
+module.exports = { getPostsAndReelsController };
