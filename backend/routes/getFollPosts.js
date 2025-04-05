087e8d5ac5ad8f24a3eb1edbd9762d014120840a
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-04-05 15:27:53 +0530

    deleted

--- backend/routes/getFollPosts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-
-const router = express.Router();
-const { getFollowingPosts } = require('../controllers/getfollowingpostsController');
-router.get('/:userId', getFollowingPosts);
-
-module.exports = router;
--- backend/routes/getFollReels.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getfollowingreels } = require('../controllers/getfollowingreelsController'); // Fixed typo here
-
-router.get('/:userId', getfollowingreels); 
-
-module.exports = router;
