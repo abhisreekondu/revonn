e956d4826b4988fbcf92254d19d73133cd1ab6c8
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-04-05 15:28:28 +0530

    modified schemas

--- backend/models/Posts.js
+++ backend/models/Posts.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  username: { type: String, required: true },
-  profilePic: { type: String, default: "" },
-  location: { type: String, required: true }, 
-  imageUrl: { type: String, required: true },
-  caption: { type: String, required: true },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
+  caption: String,
+  mediaUrl: { type: String, required: true },
+  mediaType: { type: String, enum: ['image', 'video'], required: true },
+  contentType: { type: String, enum: ['post', 'reel'], required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Users" }],
   comments: [{ username: String, text: String }],
 }, { timestamps: true });
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Posts", PostSchema);
