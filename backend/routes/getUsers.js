9d379aae5af56056ded856154fc0767e8ec5a289
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-04-05 10:47:35 +0530

    deleted

--- backend/routes/getUsers.js
+++
@@ -1,9 +0,0 @@
-// getUsers.js (Route File)
-const express = require("express");
-const router = express.Router();
-const SearchUsersController = require("../controllers/searchUsersController");
-
-// Assuming the function for fetching users is in searchUsersController.js
-router.get("/search", SearchUsersController.searchUsers);  
-
-module.exports = router;
