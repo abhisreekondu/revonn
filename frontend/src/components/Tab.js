ff9a189991165c377323cf09716244fd9c70a585
Abhi Sree Kondu <konduabhisree@gmail.com>
2025-04-18 00:19:31 +0530

    changed styles

--- frontend/src/components/Tab.js
+++ frontend/src/components/Tab.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
-import Following from '../pages/Following';
-import Explore from '../pages/Explore';
+import React, { useState } from "react";
+import { Tabs, Tab, Box } from "@mui/material";
+import Following from "../pages/Following";
+import Explore from "../pages/Explore";
 import Reels from "../pages/Reels";
-import SearchUsers from './SearchUsers';
+import SearchUsers from "./SearchUsers";
 
 const TabNavigation = () => {
   const [value, setValue] = useState(0);
@@ -13,20 +13,16 @@ const TabNavigation = () => {
   };
 
   return (
-    <Box sx={{ width: '100vw'}}>
-      
+    <Box sx={{ width: "100vw" }}>
       <Box
-  sx={{
-    position: 'fixed',
-    top: '51px', // height of the header
-    width: '100%',
-    zIndex: 1200,
-    backgroundColor: 'white',
-   
-    
-  }}
->
-      
+        sx={{
+          position: "fixed",
+          top: "51px", // height of the header
+          width: "100%",
+          zIndex: 1200,
+          backgroundColor: "white",
+        }}
+      >
         <Tabs
           value={value}
           onChange={handleChange}
@@ -34,24 +30,24 @@ const TabNavigation = () => {
           textColor="primary"
           centered
         >
-          <Tab label="Following" sx={{ color: 'red' }} />
-          <Tab label="Explore" sx={{ color: 'red' }} />
-          <Tab label="Reels" sx={{ color: 'red' }} />
+          <Tab label="Following" sx={{ color: "red" }} />
+          <Tab label="Explore" sx={{ color: "red" }} />
+          <Tab label="Reels" sx={{ color: "red" }} />
         </Tabs>
       </Box>
 
       {/* Content Based on Selected Tab */}
-      <Box sx={{ width: '100vw' }}>
-        {value === 0 && <Following />} 
-        
+      <Box sx={{ width: "100vw" }}>
+        {value === 0 && <Following />}
+
         {value === 1 && (
-          <Box sx={{ width: '100vw' , pt: '107px'}}>
-            <SearchUsers /> 
-            <Explore />    
+          <Box sx={{ width: "100vw", pt: "107px" }}>
+            <SearchUsers />
+            <Explore />
           </Box>
         )}
-        
-        {value === 2 && <Reels />}  
+
+        {value === 2 && <Reels />}
       </Box>
     </Box>
   );
